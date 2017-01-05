a1bae089791c526ada91d9f5950e7a7b25f4dc4c
DickyT <me@idickyt.com>
2017-01-05 16:05:47 -0400

    Initial Commit

---
+++ KKWords/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KKWords
+//
+//  Created by Kanzaki Mirai on 1/5/17.
+//  Copyright © 2017 Kanzaki Mirai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
